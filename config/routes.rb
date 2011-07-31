38981acb6a881b0ea5db27d37dc7eb42cd393eac
Theo Kaminskij <theodore.kaminskij@gmail.com>
2011-07-31 22:45:49 +0200

    Creato modello user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
  
-  get "users/new"
+ 
 
   #get "pages/contact"
   #get "pages/about"
@@ -10,6 +10,8 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+  
+  resources :users
   
   match '/signup', :to => 'users#new'
   #match '/', :to => 'pages/home'
