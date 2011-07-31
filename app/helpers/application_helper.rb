b25ff1775a9ea8742fd3243f6f95ca7705e278d7
Theo Kaminskij <theodore.kaminskij@gmail.com>
2011-07-31 16:24:34 +0200

    Struttura principale creata

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  #titolo predefinito
+  
+  def title
+      base_title = "Applicazione esempio Ruby"
+      if @title.nil?
+        base_title
+      else
+        "#{base_title} | #{@title}"
+      end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+ 
+  get "users/new"
 
-  get "pages/contact"
+  #get "pages/contact"
+  #get "pages/about"
+  #get "pages/help"
+  #get "pages/home"
   
-  get "pages/about"
-
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  
+  match '/signup', :to => 'users#new'
+  #match '/', :to => 'pages/home'
+  
+  root :to => 'pages#home'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
